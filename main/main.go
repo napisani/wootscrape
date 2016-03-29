3a581888c4ee3cc5595325678d4c6db1a3e959a8
napisani <napisani@yahoo.com>
2016-03-28 20:13:13 -0400

    added watch argument and a goroutine to fetch the data

--- main/main.go
+++ main/main.go
@@ -7,9 +7,29 @@ import (
 	"golang.org/x/net/html"
 	"github.com/napisani/wootscrape/matchers"
 	"github.com/napisani/wootscrape/extractors"
+	"flag"
+	"time"
 )
 
 func main() {
+	watchFrequency := flag.Int("w", 60 * 5, "the watch frequency")
+	flag.Parse();
+
+	fmt.Println(*watchFrequency);
+
+	for ;; {
+		c := make(chan string)
+		go getWootDeals(c);
+		fmt.Print("\033[H\033[2J");
+		for strDeal := range c {
+			fmt.Print(strDeal);
+		}
+		time.Sleep(time.Duration(*watchFrequency)*time.Second);
+	}
+}
+
+func getWootDeals(c chan string){
+
 	// request and parse the front page
 	resp, err := http.Get("http://woot.com")
 	if err != nil {
@@ -21,17 +41,19 @@ func main() {
 	}
 
 	// define a matcher
-	matcher := scrape.Matcher(matchers.GetWootOfferNode());
+	matcher := scrape.Matcher(matchers.GetWootOfferNode())
 	// grab all articles and print them
 	offerNodes := scrape.FindAll(root, matcher)
 	for _, offerNode := range offerNodes {
-		offer := extractors.GetWootDeal(offerNode);
+		offer := extractors.GetWootDeal(offerNode)
 		if offer.MaxPrice != offer.MinPrice {
-			fmt.Printf("%20s) %60s :: %s - %s\n", offer.Category, offer.Title, offer.MinPrice, offer.MaxPrice);
+			c <- fmt.Sprintf("%20s) %60s :: %s - %s\n", offer.Category, offer.Title, offer.MinPrice, offer.MaxPrice)
 		}else{
-			fmt.Printf("%20s) %60s :: %s\n", offer.Category, offer.Title, offer.MinPrice);
+			c <- fmt.Sprintf("%20s) %60s :: %s\n", offer.Category, offer.Title, offer.MinPrice)
 
 		}
 
 	}
+	close(c)
+
 }
